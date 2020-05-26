fdc51301f8c2f222307322747f2a03ecd42dc866
Serg Klukvin <klukvin@ukr.net>
2020-05-26 21:02:17 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// let arr = [
+//   "https://www.youtube.com/embed/Hu4dzKVdcHQ",
+//   "https://www.youtube.com/embed/fwvxtbsP1dE",
+//   "https://www.youtube.com/embed/5GGEp6Lghqg",
+// ];
+
+// let curentSeria = 1;
+
+// function myClick(el) {
+//   if (curentSeria - 1 === el) {  //// перевірка щоб користувач не натискав одну і ту саму серію декілька разів підряд
+//     return;
+//   }
+
+//   curentSeria = el + 1;
+
+//   document.getElementById("result").src = arr[el];
+// }
+
+
+
